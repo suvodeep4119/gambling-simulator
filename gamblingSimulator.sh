7666a35e7c676a4734bb9334f4d592e7303e18a5
suvodeep4119 <suvodeep4119@gmail.com>
2020-02-07 21:15:20 +0530

    MODIFIED: Made extensive changes to the structure

--- gamblingSimulator.sh
+++ gamblingSimulator.sh
@@ -1,98 +1,109 @@
-#!/bin/bash -x
+#!/bin/bash -x 
 
 echo "Welcome to THE GAMBLING SIMULATOR"
 
 #CONSTANTS
-STAKE=6
+STAKE=5
 BET=1
 WIN=1
-WINNING_STAKE_LIMIT=$((STAKE+(( (50*6)/100 )) ))
-LOSING_STAKE_LIMIT=$((STAKE-(( (50*6)/100 )) ))
+WINNING_STAKE_LIMIT=$((STAKE+(( (50*STAKE)/100 )) ))
+LOSING_STAKE_LIMIT=$((STAKE-(( (50*STAKE)/100 )) ))
 PLAY_PERIOD=3
-
-#DICTIONARY
-declare -A dailyWon
-declare -A dailyLoss
 
 #VARIABLES
 dailyCash=$STAKE
-dailyWonAmount=0
-wonAmountTotal=0
-dailyLostAmount=0
-lostAmountTotal=0
-daysWonCounter=0
-daysLostCounter=0
+dailyWonBetCounter=0
+dailyLostBetCounter=0
+totalDailyWonBetCounter=0
+totalDailyLostBetCounter=0
+
 #FUNCTIONS
 
-#Function for EACH BET taking place
-function placeBet()
+#1
+function placeBet() 
 {
 	winLoss=$((RANDOM%2))
 	if [ $winLoss -eq $WIN ]
 	then
+		echo "Won Bet"
+        ((dailyWonBetCounter++))
+        ((totalDailyWonBetCounter++))
 		((dailyCash++))
-		echo $dailyCash
-		((dailyWonAmount++))
-		echo $dailyWonAmount
+        echo "Balance :$dailyCash"
+        echo ++++++++++
 	else
+		echo "Lost Bet"
+        ((dailyLostBetCounter++))
+        ((totalDailyLostBetCounter++))
 		((dailyCash--))
-		echo $dailyCash
-		((dailyLostAmount++))
-		echo "$dailyLostAmount"
+        echo "Balance:$dailyCash"
+        echo ++++++++++
 	fi
 }
 
-function totalAmountWonLoss()
+#2
+function dailyPlay()
 {
-	if [ $wonAmountTotal -gt $lostAmountTotal ]
-	then
-		echo "You Won !! "
-		(($wonAmountTotal))
-		echo "Total days won: " $(($daysWonCounter))
-		echo "Amount won per day:"  ${!dailyWon[@]}
-	else
-		echo "You Lost !! "
-		(($lostAmountTotal))
-		echo "Total days lost: " $(($daysLostCounter))
-		echo "Amount lost per day: " ${!dailyLoss[@]}
-	fi
+    while (( $dailyCash > $LOSING_STAKE_LIMIT && $dailyCash < $WINNING_STAKE_LIMIT ))
+    do
+        placeBet
+    done
+    reachLimitMessage
+    echo "+++++++++++++++++++++++++++++++"
+    echo "Re-initializing dailyCash value,won & lost Amount"
+    dailyCash=$STAKE
+    dailyWonBetCounter=0
+    dailyLostBetCounter=0
 }
 
-function dailyWonLoss()
+#3
+function reachLimitMessage()
 {
-	
-	if [ $dailyWonAmount -gt $dailyLostAmount ]
-	then
-		echo "You won for the day"
-		((daysWonCounter++))
-		#dailyWon[$day]=$dailyWonAmount
-	else
-		echo "You lost for the day"
-		((daysLostCounter++))
-		#dailyLoss[$day]=$dailyLostAmount
-	fi
+    if (( $dailyCash == $LOSING_STAKE_LIMIT))
+    then    
+        echo "You reached Minimums"
+        
+        echo "Daily Cash remaining for the day: $dailyCash"
+        echo "You LOST for the day"
+    else    
+        echo "You reached maximum"
+        
+        echo "Daily Cash remaining fo the day: $dailyCash"
+        echo "You Won for the day"
+    fi
+
+    echo "Amount Lost for the day: $dailyLostBetCounter"
+    echo "Amount Won for the day: $dailyWonBetCounter"
 }
+
+#4
+function periodicPlay()
+{
+    for (( day=1; day<=$PLAY_PERIOD; day++ ))
+    do
+        dailyPlay
+    done
+    echo "After playing $PLAY_PERIOD days : "
+    echo "Total Amount WON: $totalDailyWonBetCounter"
+    echo "Total Amount LOST: $totalDailyLostBetCounter"
+    echo "********************************************"
+    if (( $totalDailyWonBetCounter>$totalDailyLostBetCounter ))
+    then    
+        echo " YOU WON !!"
+    else
+        echo "YOU LOST !!"
+    fi
+}
+
+#5
+function play()
+{
+    periodicPlay
+    
+}
+
+
+
 
 #MAIN
-#Loop for 20 days
-for (( day=1; day<=$PLAY_PERIOD; day++ ))
-do
-	#Loop for 1-day/per-day
-	while (( $dailyCash > $LOSING_STAKE_LIMIT && $dailyCash < $WINNING_STAKE_LIMIT ))
-	do
-		placeBet
-	done
-	#Reflect dailyCash & daily Won/Loss Amount at day's end and post message
-	echo "Daily Cash" $dailyCash
-	(($dailyWonAmount))
-	(($dailyLostAmount))
-	dailyWonLoss
-	wonAmountTotal=$(( $wonAmountTotal+$dailyWonAmount ))
-	dailyWon[$daysWonCounter]=$dailyWonAmount
-	lostAmountTotal=$(( $lostAmountTotal+$dailyLostAmount ))
-	dailyLoss[$daysLostCounter]=$dailyLostAmount
-	dailyCash=$STAKE
-done
-totalAmountWonLoss
-
-
+play

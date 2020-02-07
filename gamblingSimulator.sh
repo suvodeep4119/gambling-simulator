fc0ef955ce1b20c7aa7cff650c310245ee7725e9
suvodeep4119 <suvodeep4119@gmail.com>
2020-02-07 15:23:10 +0530

    WIP: Fixing the dictionary used for won/Loss display

--- gamblingSimulator.sh
+++ gamblingSimulator.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -x 
+#!/bin/bash -x
 
 echo "Welcome to THE GAMBLING SIMULATOR"
 
@@ -9,12 +9,19 @@ WIN=1
 WINNING_STAKE_LIMIT=$((STAKE+(( (50*100)/100 )) ))
 LOSING_STAKE_LIMIT=$((STAKE-(( (50*100)/100 )) ))
 PLAY_PERIOD=20
+
+#DICTIONARY
+declare -A dailyWon
+declare -A dailyLoss
+
 #VARIABLES
 dailyCash=$STAKE
 dailyWonAmount=0
 wonAmountTotal=0
 dailyLostAmount=0
 lostAmountTotal=0
+daysWonCounter=0
+daysLostCounter=0
 #FUNCTIONS
 
 #Function for EACH BET taking place
@@ -23,11 +30,9 @@ function placeBet()
 	winLoss=$((RANDOM%2))
 	if [ $winLoss -eq $WIN ]
 	then
-		echo "Win"
 		((dailyCash++))
 		((dailyWonAmount++))
 	else
-		echo "Loss"
 		((dailyCash--))
 		((dailyLostAmount++))
 	fi
@@ -39,26 +44,49 @@ function totalAmountWonLoss()
 	then
 		echo "You Won !! "
 		(($wonAmountTotal))
+		echo "Total days won: " $(($daysWonCounter))
+		echo "Amount won per day:"  ${!dailyWon[@]}
 	else
 		echo "You Lost !! "
 		(($lostAmountTotal))
+		echo "Total days lost: " $(($daysLostCounter))
+		echo "Amount lost per day: " ${!dailyLoss[@]}
 	fi
 }
+
+function dailyWonLoss()
+{
+	if [ $dailyWonAmount -gt $dailyLostAmount ]
+	then
+		echo "You won for the day"
+		((daysWonCounter++))
+		#dailyWon[$day]=$dailyWonAmount
+	else
+		echo "You lost for the day"
+		((daysLostCounter++))
+		#dailyLoss[$day]=$dailyLostAmount
+	fi
+}
+
 #MAIN
 #Loop for 20 days
 for (( day=1; day<=$PLAY_PERIOD; day++ ))
 do
 	#Loop for 1 day/per day
-	while (( $dailyCash > $LOSING_STAKE_LIMIT && $dailyCash < WINNING_STAKE_LIMIT ))
+	while (( $dailyCash > $LOSING_STAKE_LIMIT && $dailyCash < $WINNING_STAKE_LIMIT ))
 	do
 		placeBet
 	done
-	echo "You're done for the day"
 	#Reflect dailyCash & daily Won/Loss Amount at day's end and post message
 	(($dailyCash))
 	(($dailyWonAmount))
-	(($dailyLostAmount)) 
+	(($dailyLostAmount))
+	dailyWonLoss
 	wonAmountTotal=$(( $wonAmountTotal+$dailyWonAmount ))
+	dailyWon[$daysWonCounter]=$dailyWonAmount
 	lostAmountTotal=$(( $lostAmountTotal+$dailyLostAmount ))
+	dailyLoss[$daysLostCounter]=$dailyLostAmount
 done
 totalAmountWonLoss
+
+

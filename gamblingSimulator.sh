d5252d5fc071d12dd68dc1612f6ff40453f34de1
suvodeep4119 <suvodeep4119@gmail.com>
2020-02-07 09:48:32 +0530

    ADDED: Win/Loss update for dailyCash

--- gamblingSimulator.sh
+++ gamblingSimulator.sh
@@ -5,5 +5,20 @@ echo "Welcome to THE GAMBLING SIMULATOR"
 #CONSTANTS
 STAKE=100
 BET=1
+WIN=1
+#VARIABLES
+dailyCash=$STAKE
 
-
+function placeBet()
+{
+	winLoss=$((RANDOM%2))
+	if [ $winLoss -eq $WIN ]
+	then
+		echo "Win"
+		((dailyCash++))
+	else
+		echo "Loss"
+		((dailyCash--))
+	fi
+}
+placeBet

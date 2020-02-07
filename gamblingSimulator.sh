b3794699b00960e7c64024a9512944a1815815b6
suvodeep4119 <suvodeep4119@gmail.com>
2020-02-07 13:05:32 +0530

    ADDED: Feature to Display Total Win/Loss at Period end

--- gamblingSimulator.sh
+++ gamblingSimulator.sh
@@ -8,11 +8,16 @@ BET=1
 WIN=1
 WINNING_STAKE_LIMIT=$((STAKE+(( (50*100)/100 )) ))
 LOSING_STAKE_LIMIT=$((STAKE-(( (50*100)/100 )) ))
-
+PLAY_PERIOD=20
 #VARIABLES
 dailyCash=$STAKE
-
+dailyWonAmount=0
+wonAmountTotal=0
+dailyLostAmount=0
+lostAmountTotal=0
 #FUNCTIONS
+
+#Function for EACH BET taking place
 function placeBet()
 {
 	winLoss=$((RANDOM%2))
@@ -20,18 +25,40 @@ function placeBet()
 	then
 		echo "Win"
 		((dailyCash++))
+		((dailyWonAmount++))
 	else
 		echo "Loss"
 		((dailyCash--))
+		((dailyLostAmount++))
 	fi
 }
 
+function totalAmountWonLoss()
+{
+	if [ $wonAmountTotal -gt $lostAmountTotal ]
+	then
+		echo "You Won !! "
+		(($wonAmountTotal))
+	else
+		echo "You Lost !! "
+		(($lostAmountTotal))
+	fi
+}
 #MAIN
-while (( $dailyCash > $LOSING_STAKE_LIMIT && $dailyCash < WINNING_STAKE_LIMIT ))
+#Loop for 20 days
+for (( day=1; day<=$PLAY_PERIOD; day++ ))
 do
-	placeBet
+	#Loop for 1 day/per day
+	while (( $dailyCash > $LOSING_STAKE_LIMIT && $dailyCash < WINNING_STAKE_LIMIT ))
+	do
+		placeBet
+	done
+	echo "You're done for the day"
+	#Reflect dailyCash & daily Won/Loss Amount at day's end and post message
+	(($dailyCash))
+	(($dailyWonAmount))
+	(($dailyLostAmount)) 
+	wonAmountTotal=$(( $wonAmountTotal+$dailyWonAmount ))
+	lostAmountTotal=$(( $lostAmountTotal+$dailyLostAmount ))
 done
-
-#Reflect dailyCash at day's end and post message
-(($dailyCash)) 
-echo "You're done for the day"
+totalAmountWonLoss
